e451ade2fd67c4152ff95452f76a9def90c67186
Java <sergeyveletnyuk57@gmail.com>
2023-10-10 22:17:54 +0300

    rew.rar

---
+++ HomeWork/Homework_num_7/task1.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def group_rename_files(new_name: str, ext_renamed: str, ext_new: str, saved_range: tuple):
+    work_path = Path.cwd()
+    count_renamed = 0
+    for i in work_path.iterdir():
+        if i.is_file() and i.suffix == ext_renamed:
+            file_name = f"{i.stem[saved_range[0]:saved_range[1]]}{new_name}_{count_renamed + 1:03}{ext_new}"
+            i.rename(Path(i.parent, file_name))
+            count_renamed += 1
+
+    print(count_renamed)
+
+
+if __name__ == '__main__':
+    group_rename_files("task", ".pdf", ".txt", (1, 3))
